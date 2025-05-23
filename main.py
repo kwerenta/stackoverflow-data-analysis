20536f5d3e3b92085d524ab12d05b4fa6af37300
Kamil Wenta <kamil.wenta222@gmail.com>
2025-05-23 21:40:44 +0200

    Add spearman correlation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.stats import spearmanr
 
 df = pd.read_csv("data.csv")
 
@@ -23,9 +25,11 @@ attributes = [
     "HaveWorkedPlatform"
 ]
 
+df_nominal = df[attributes].dropna()
+
 # Draw histograms (attribute values distribution)
 for attr in attributes:
-    attr_counts = df[attr].value_counts().head(20)
+    attr_counts = df_nominal[attr].value_counts().head(20)
     plt.figure(figsize = (20,20))
     plt.bar(attr_counts.index, attr_counts.values)
     plt.xlabel(attr)
@@ -36,3 +40,14 @@ for attr in attributes:
     plt.savefig(f"figures/histograms/{attr}.png")
     plt.show()
 
+# Encode values of nominal attributes as integers
+df_encoded = df_nominal.apply(lambda x: pd.factorize(x)[0])
+corr_matrix = spearmanr(df_encoded)[0]
+corr_df = pd.DataFrame(corr_matrix, index=attributes, columns=attributes)
+
+plt.figure(figsize = (16,16))
+sns.heatmap(corr_df, annot=True, fmt=".2f", cmap="coolwarm", cbar=True)
+plt.title("Macierz korelacji metodą Spearmana")
+plt.tight_layout
+plt.savefig(f"figures/spearman_corr.png")
+plt.show()
