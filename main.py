98259648e546f954c9034825f389624739f679a5
Kamil Wenta <kamil.wenta222@gmail.com>
2025-05-26 18:08:16 +0200

    Add Salary outliers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from matplotlib.pyplot import figtext
 from scipy.stats import spearmanr
 
 df = pd.read_csv("data.csv")
@@ -66,6 +67,24 @@ for attr in nominal_attrs:
     plt.tight_layout()
     plt.savefig(f"figures/histograms/{attr}.png")
     plt.show()
+
+# Calculate salary outliers
+valid_idx = df_nominal.dropna().index.intersection(df.index)
+salary = df.loc[valid_idx, ["Salary"]]["Salary"]
+plt.figure(figsize=(16, 12))
+sns.boxplot(x=salary.values)
+plt.xlabel("Salary")
+plt.title(f"Punkty oddalone atrybutu Salary")
+plt.savefig(f"figures/boxplot_Salary.png")
+plt.show()
+
+Q1 = salary.quantile(0.25)
+Q3 = salary.quantile(0.75)
+IQR = Q3 - Q1
+lower = Q1 - 1.5 * IQR
+upper = Q3 + 1.5 * IQR
+outliers = salary[(salary < lower) | (salary > upper)]
+print(f"Salary - punkty oddalone\n\t{salary.describe()}\n\tLiczba outlierów = {len(outliers)}")
 
 # Encode values of nominal attributes as integers
 df_encoded = df_nominal.apply(lambda x: pd.factorize(x)[0])
